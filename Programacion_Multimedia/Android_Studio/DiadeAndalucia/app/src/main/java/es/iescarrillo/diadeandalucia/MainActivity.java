c66c4cc7289165a5725bdb0d913d06a89af88a6b
h4ck-p0wn3d <msanbar104g@iescarrillo.es>
2025-09-26 19:04:19 +0200

    Update MainActivity.java

--- Programacion_Multimedia/Android_Studio/DiadeAndalucia/app/src/main/java/es/iescarrillo/diadeandalucia/MainActivity.java
+++ Programacion_Multimedia/Android_Studio/DiadeAndalucia/app/src/main/java/es/iescarrillo/diadeandalucia/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
 
     MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +31,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
 
-            Button boton=(Button)findViewById(R.id.button);
+            Button boton = (Button) findViewById(R.id.button);
             boton.setOnClickListener(this);
 
-
-            mp=MediaPlayer.create(this, R.raw.himno_de_andalucia);
+            mp = MediaPlayer.create(this, R.raw.himno_de_andalucia);
             return insets;
 
         });
@@ -41,39 +42,39 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-    mp.start();
+        mp.start();
         Toast.makeText(getApplicationContext(), "Feliz Día de Andalucía", Toast.LENGTH_SHORT).show();
     }
 
     protected void onDestroy() {
         super.onDestroy();
         mp.stop();
-        Log.i("Estado","Actividad Destruida");
+        Log.i("Estado", "Actividad Destruida");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("Estado","Actividad Ejecutada");
+        Log.i("Estado", "Actividad Ejecutada");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("Estado","Actividad Iniciada");
+        Log.i("Estado", "Actividad Iniciada");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mp.stop();
-        Log.i("Estado","Actividad Pausada");
+        Log.i("Estado", "Actividad Pausada");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("Estado","Actividad Detenida");
+        Log.i("Estado", "Actividad Detenida");
 
     }
 }
