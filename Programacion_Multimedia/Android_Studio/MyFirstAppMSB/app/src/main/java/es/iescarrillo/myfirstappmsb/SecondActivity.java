1b90c4452d0bcfdbbf9a229d6d72999de0dae9dc
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-10 11:29:55 +0200

    Android Studio

--- Programacion_Multimedia/Android_Studio/MyFirstAppMSB/app/src/main/java/es/iescarrillo/myfirstappmsb/SecondActivity.java
+++ Programacion_Multimedia/Android_Studio/MyFirstAppMSB/app/src/main/java/es/iescarrillo/myfirstappmsb/SecondActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnEnviarDatos;
+    EditText tvTexto;
+    EditText tvEntero;
+    EditText tvDecimal;
+    Switch switchBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +32,31 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnEnviarDatos = (Button) findViewById(R.id.buttonEnviarDatos);
+        tvTexto = (EditText) findViewById(R.id.editText);
+        tvEntero = (EditText) findViewById(R.id.editTextNumber);
+        tvDecimal = (EditText) findViewById(R.id.editTextNumberDecimal);
+        switchBoolean = (Switch) findViewById(R.id.switchBoolean);
+
+        btnEnviarDatos.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        //Boton siguiente que manda a la ThirdActivity
+        if (findViewById(R.id.buttonEnviarDatos).isPressed()) {
+            //Creamos un Intent para ir a la siguiente activity
+            Intent viewNameIntent = new Intent(this, ThirdActivity.class);
 
+            viewNameIntent.putExtra("name", tvTexto.getText().toString());
+            viewNameIntent.putExtra("numero", tvEntero.getText().toString());
+            viewNameIntent.putExtra("numeroDecimal", tvDecimal.getText().toString());
+            viewNameIntent.putExtra("boolean", switchBoolean.isChecked());
+
+            //Inicia la Actividad
+            startActivity(viewNameIntent);
+        }
 
     }
 }
---
+++ Programacion_Multimedia/Android_Studio/MyFirstAppMSB/app/src/main/java/es/iescarrillo/myfirstappmsb/ThirdActivity.java
@@ -0,0 +1,59 @@
+package es.iescarrillo.myfirstappmsb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnVolver;
+    TextView tvDatos, tvDatos2, tvDatos3, tvDatos4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvDatos = findViewById(R.id.textViewDatos);
+        tvDatos2 = findViewById(R.id.textViewDatos2);
+        tvDatos3 = findViewById(R.id.textViewDatos3);
+        tvDatos4 = findViewById(R.id.textViewSwitch);
+
+        btnVolver = findViewById(R.id.buttonVolver);
+        btnVolver.setOnClickListener(this);
+
+        Intent viewMainIntent = getIntent();
+        if (viewMainIntent != null) {
+            tvDatos.setText("Texto: " + viewMainIntent.getStringExtra("name"));
+            tvDatos2.setText("Número entero: " + viewMainIntent.getStringExtra("numero"));
+            tvDatos3.setText("Número decimal: " + viewMainIntent.getStringExtra("numeroDecimal"));
+
+            boolean estadoSwitch = viewMainIntent.getBooleanExtra("boolean", false);
+            tvDatos4.setText("Switch activado: " + estadoSwitch);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonVolver) {
+            Intent viewNameIntent = new Intent(this, MainActivity.class);
+            startActivity(viewNameIntent);
+        }
+    }
+}
