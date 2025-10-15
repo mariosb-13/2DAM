c103291bcc102b0545b2123d39250f063de38a20
h4ck-p0wn3d <msanbar104g@iescarrillo.es>
2025-10-15 14:02:07 +0200

    PSP

--- Programacion_Servicios/Tema 1/PSP_Tema1_EjemplosC/PSP_UA1_Tarea2_MarioSanchezBarroso.c
+++ Programacion_Servicios/Tema 1/PSP_Tema1_EjemplosC/PSP_UA1_Tarea2_MarioSanchezBarroso.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-#include <unistd.h>   // Para fork()
-#include <sys/types.h> // Para pid_t
-#include <sys/wait.h>  // Para wait()
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
-void MySignalHandler(int sig){
-    printf("Signal number is %d \n ", sig);
+void MySignalHandler(int sig) {
+    char respuesta;
+    printf("\nHe recibido la señal %d\n", sig);
+    printf("¿Quieres terminar el proceso? (s/n): ");
+    scanf(" %c", &respuesta);
+
+    if (respuesta == 's' || respuesta == 'S') {
+        printf("Finalizando el proceso...\n");
+        exit(0);
+    } else {
+        printf("Continuando la ejecución...\n");
+    }
 }
+
 int main() {
-   signal(SIGIO, &MySignalHandler);
-   while (1)
-   {
-    printf("zzzzZZZZzzz \n");
-    sleep(2);
-   }
-   
+    signal(SIGUSR1, MySignalHandler);
+    while (1) {
+        printf("Proceso activo...\n");
+        sleep(2);
+    }
+    return 0;
 }
