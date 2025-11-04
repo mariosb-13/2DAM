8a1ebe27ea231c2e5797655538e4469da47088ab
h4ck-p0wn3d <msanbar104g@iescarrillo.es>
2025-11-04 19:11:57 +0100

    Ejercicio PS 6

--- Programacion_Servicios/PS_Eclipse/Tema_1/tareas/tarea_6/Argumentos.java
+++ Programacion_Servicios/PS_Eclipse/Tema_1/tareas/tarea_6/Argumentos.java
@@ -3,13 +3,23 @@ package tarea_6;
 public class Argumentos {
 
 	public static void main(String[] args) {
-	
-		if (args.length<1) {
+		// Si no hay argumentos
+		if (args.length < 1) {
 			System.exit(1);
-		}else {
-			
 		}
-		
-	}
 
+		String arg = args[0];
+
+		try {
+			int numero = Integer.parseInt(arg);
+			if (numero < 0) {
+				System.exit(3);
+			} else {
+				System.exit(0);
+			}
+		} catch (NumberFormatException e) {
+			// No es un número entero
+			System.exit(2);
+		}
+	}
 }
--- Programacion_Servicios/PS_Eclipse/Tema_1/tareas/tarea_6/Main.java
+++ Programacion_Servicios/PS_Eclipse/Tema_1/tareas/tarea_6/Main.java
@@ -7,19 +7,45 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			ProcessBuilder pb = new ProcessBuilder("java","tarea_6.Argumentos");
+			ProcessBuilder pb = new ProcessBuilder("java", "tarea_6.Argumentos");
+
+			// Agregamos los argumentos pasados a este programa
+			if (args.length > 0) {
+				for (String arg : args) {
+					pb.command().add(arg);
+				}
+			}
+					
 			pb.directory(new File("bin"));
 			pb.inheritIO();
+
+
 			Process p = pb.start();
-			p.waitFor();
-			
+			int exitCode = p.waitFor(); // Espera a que termine
+
+			// Mostrar el resultado según el código de salida
+			switch (exitCode) {
+			case 0:
+				System.out.println("El programa finalizó correctamente (código 0).");
+				break;
+			case 1:
+				System.out.println("Error: No se proporcionaron argumentos (código 1).");
+				break;
+			case 2:
+				System.out.println("Error: El argumento no es un número entero (código 2).");
+				break;
+			case 3:
+				System.out.println("Error: El número entero es menor que 0 (código 3).");
+				break;
+			default:
+				System.out.println("Error desconocido (código " + exitCode + ").");
+				break;
+			}
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 }
