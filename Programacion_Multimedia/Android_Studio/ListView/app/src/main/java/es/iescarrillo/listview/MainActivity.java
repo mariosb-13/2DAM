dd3400100171b21c59bcc2010c3e2f5640bd71b4
mariosb_13 <msanbar104g@iescarrillo.es>
2025-11-06 11:27:21 +0100

    ListView

---
+++ Programacion_Multimedia/Android_Studio/ListView/app/src/main/java/es/iescarrillo/listview/MainActivity.java
@@ -0,0 +1,52 @@
+package es.iescarrillo.listview;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        lv = findViewById(R.id.listView);
+
+        // Click normal
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundColor(getColor(R.color.pink)); // rosa
+            }
+        });
+
+        // Click prolongado
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundColor(getColor(R.color.white)); // blanco
+                return true; // ← importante: true indica que el evento se ha consumido
+            }
+        });
+    }
+}
